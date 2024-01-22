6ed1c32efef4a13389538c2b240ad89294ecbeb6
zzriku <zzriku@users.noreply.github.com>
2024-01-22 05:57:41 +0000

    javascript

--- app/views/javascript/script.js
+++ app/views/javascript/script.js
@@ -1,13 +1,10 @@
-let hello = "Hello, World";
-alert(hello);
+let orange = 100;
+let apple = 120;
 
-let int1= 1;
-alert(int1);
-
-alert(4+3);
-
-alert("Hello"+"World");
-
-let str1 = "Hello";
-let str2 = "world!!";
-alert(str1 + str2);
+if (orange < apple){
+  alert("みかんの値段がりんごより安い");
+} else if(orange == apple){
+  alert("みかんとりんごが同じ値段");
+} else{
+  alert("みかんの値段がりんごより高い");
+}
