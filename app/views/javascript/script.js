74aba7fbfd67f441028f5fc24002ef2a3d8ede20
zzriku <zzriku@users.noreply.github.com>
2024-01-22 05:54:09 +0000

    javascript

--- app/views/javascript/script.js
+++ app/views/javascript/script.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+let hello = "Hello, World";
+alert(hello);
+
+let int1= 1;
+alert(int1);
+
+alert(4+3);
+
+alert("Hello"+"World");
+
+let str1 = "Hello";
+let str2 = "world!!";
+alert(str1 + str2);
